cc5a6cb2701c3eb2e0eb68631aca74aac027e7fb
LFerrat <ferratlauric@gmail.com>
2021-06-03 09:33:15 +0200

    adding draft help_shortcut function

---
+++ R/help_shortcut.R
@@ -0,0 +1,11 @@
+#' Title
+#'
+#' @return
+#' @export automatic call of help on the function where the pointer is
+#'
+#' @examples
+help_shortcut <- function() {
+
+}
+# https://rstudio.github.io/rstudioapi/reference/index.html
+# https://rstudio.github.io/rstudioapi/reference/sendToConsole.html
